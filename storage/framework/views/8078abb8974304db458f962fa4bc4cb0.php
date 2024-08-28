e544c814426f38a59a7e9ee0c8ae506af50af83f
Ibrahim Alqarni <ibrahimalqarrni@gmail.com>
2024-08-28 23:38:31 +0300

    Delete storage directory

--- storage/framework/views/8078abb8974304db458f962fa4bc4cb0.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('title','Edit Task'); ?>
-
-
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('form',['task' => $task], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\abukh\laravel10\l10-task-list\resources\views/edit.blade.php ENDPATH**/ ?>
